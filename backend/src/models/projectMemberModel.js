61690e6a8d68a4c2efd22b01139f107fed367241
BROADSTREETBULLY <hjw.personal@gmail.com>
2026-07-20 16:49:21 +1000

    - timestamps added to projectModel _ projectMemberModel complete

--- backend/src/models/projectMemberModel.js
+++ backend/src/models/projectMemberModel.js
@@ -1,12 +1,16 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const ProjMembershipSchema = new Schema({
+const ProjectMemberSchema = new Schema(
+  {
+    member: { type: Schema.Types.ObjectId, ref: "User", required: true },
+    projectID: { type: Schema.Types.ObjectId, ref: "Project", required:true },
+    roleOnProject: { type: String, trim: true },
+    addedBy: { type: Schema.Types.ObjectId, ref: "User" },
+  },
+  { timestamps: true },
+);
 
+ProjectMemberSchema.index({ member: 1, projectID: 1 }, { unique: true }); //Ensure same user can't be added twice to one project 
 
-
-});
-
-
-
-module.exports = mongoose.model("ProjMembership", ProjMembershipSchema);
+module.exports = mongoose.model("ProjectMember", ProjectMemberSchema);
