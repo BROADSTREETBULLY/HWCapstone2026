43b65c202241181aea302e92591c65e3ad1bb7fa
BROADSTREETBULLY <hjw.personal@gmail.com>
2026-07-20 17:12:39 +1000

    ScheduleItem complete
    slight updates to other models

--- backend/src/models/specOptModel.js
+++ backend/src/models/specOptModel.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const SpecOptSchema = new Schema({
+const SpecOptionSchema = new Schema({
 
 
 
@@ -9,4 +9,4 @@ const SpecOptSchema = new Schema({
 
 
 
-module.exports = mongoose.model("SpecOpt", SpecOptSchema);
+module.exports = mongoose.model("SpecOption", SpecOptionSchema);
--- backend/src/models/userModel.js
+++ backend/src/models/userModel.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const userSchema = new Schema(
+const UserSchema = new Schema(
   {
     username: { type: String, trim: true, required: true, unique: true },
     orgId: { type: Schema.Types.ObjectId, ref: "Organisation", required: true },
@@ -20,4 +20,4 @@ const userSchema = new Schema(
   { timestamps: true },
 );
 
-module.exports = mongoose.model("User", userSchema);
+module.exports = mongoose.model("User", UserSchema);
