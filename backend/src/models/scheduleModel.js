8ed89905a04415147f65126497a4719e9221b073
BROADSTREETBULLY <hjw.personal@gmail.com>
2026-07-20 16:59:00 +1000

    - schedule schema created

--- backend/src/models/scheduleModel.js
+++ backend/src/models/scheduleModel.js
@@ -1,12 +1,18 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const ScheduleSchema = new Schema({
-
-
-
-});
-
-
+const ScheduleSchema = new Schema(
+  {
+    projectID: { type: Schema.Types.ObjectId, ref: "Project", required: true },
+    scheduleType: { type: String, trim: true, required: true },
+    scheduleTitle: { type: String, trim: true },
+    scheduleStatus: { type: String, trim: true, default: "draft" },
+    createdBy: { type: Schema.Types.ObjectId, ref: "User" },
+    updatedBy: { type: Schema.Types.ObjectId, ref: "User" },
+    scheduleDescription: { type: String, trim: true },
+    scheduleComments: { type: String, trim: true },
+  },
+  { timestamps: true },
+);
 
 module.exports = mongoose.model("Schedule", ScheduleSchema);
