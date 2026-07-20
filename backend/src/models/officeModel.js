cddb215b6072f2195360ad7e97ad40abecb47146
BROADSTREETBULLY <hjw.personal@gmail.com>
2026-07-20 15:50:47 +1000

    Office Schema created

--- backend/src/models/officeModel.js
+++ backend/src/models/officeModel.js
@@ -1,12 +1,18 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const OfficeSchema = new Schema({
+const OfficeSchema = new Schema(
+  {
+    orgId: { type: Schema.Types.ObjectId, ref: "Organisation", required: true },
+    officeName: { type: String, trim: true, required: true },
+    state: { type: String, trim: true, required: true },
+    address: { type: String, trim: true },
+    phone: { type: String, trim: true },
+    isActive: { type: Boolean, default: true },
+  },
+  { timestamps: true },
+);
 
-
-
-});
-
-
+OfficeSchema.index({ orgId: 1, officeName: 1 }, { unique: true });
 
 module.exports = mongoose.model("Office", OfficeSchema);
